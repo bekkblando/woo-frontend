9f770e0ca3402188060b8d43c0f84dcce9b5e542
Bekk Blando <bekkblando@Mac.home>
2025-11-12 20:14:50 +0100

    fix

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -42,7 +42,7 @@ const Landing = () => {
             <div className="text-[15px] font-bold">
                 VraagMijnOverheid
             </div>
-            <img src="/public/government-logo.png" alt="Woo Logo" className="h-12" />
+            <img src="/government-logo.png" alt="Woo Logo" className="h-12" />
             <div className="text-sm text-[#154475]">
                 <IconUser className="inline-block"/>
                 <span className="inline-block ml-2 justify-center items-center pt-1">Inloggen</span>
